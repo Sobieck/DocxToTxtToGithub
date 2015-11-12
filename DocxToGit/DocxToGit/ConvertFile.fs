1062f40d43e7922da49f6e3aa160724ce5e62675
Sobieck00 <thomas@sobieck.com>
2015-11-12 06:51:47 -0600

    Can create txt files. Trying to make a commit and then sync.

---
+++ DocxToGit/DocxToGit/ConvertFile.fs
@@ -0,0 +1,16 @@
+﻿
+
+module ConvertFile
+
+open Spire.Doc
+
+
+    let convertDocument path targetFileName extensionOfFileToConvert = 
+        let doc = new Document()
+        
+        let fileToConvertPath = path + targetFileName + extensionOfFileToConvert
+        doc.LoadFromFile(fileToConvertPath)
+        
+
+        let txtifiedFilePath = fileToConvertPath.Replace(extensionOfFileToConvert, ".txt")
+        doc.SaveToFile(txtifiedFilePath, FileFormat.Txt)
---
+++ DocxToGit/DocxToGit/Program.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+module DocxToGit.main
+
+open System
+open ConvertFile
+open UpdateRepo
+
+
+ [<EntryPoint>]
+let main argv = 
+    printf "Hello"
+
+    let arguments = Array.toList argv
+
+    let extension = List.tail arguments |> List.tail |> List.head
+    let fileName = List.tail arguments |> List.head
+
+    convertDocument (List.head arguments) fileName extension
+      
+ 
+    let result = github (List.last arguments)
+
+    let input = Console.ReadLine()
+
+    Console.Write("You entered: {0}", input)
+ 
+    let nextInput = Console.ReadLine() |> ignore
+    0
+
